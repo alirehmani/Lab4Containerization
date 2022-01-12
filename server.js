8cc2eec22c7ac3fb66f28ddf8b952b4ad4e62fb4
Osman Ale <osman.highbrow@gmail.com>
2022-01-12 13:46:52 +0500

    line indentation

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var handleRequest = function(request, response) {
   response.end(" | v=0\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 
+
+
 var www = http.createServer(handleRequest);
 www.listen(8080,function () {
     startTime = new Date();;
