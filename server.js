41a2d7d0d7841dce339705460638d4b2402d874c
osmanale <osman.highbrow@gmail.com>
2022-01-13 01:33:56 +0500

    app changes

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ var handleRequest = function(request, response) {
   response.end(" | v=0\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 var www = http.createServer(handleRequest);
+
 www.listen(8080,function () {
     startTime = new Date();;
     host = process.env.HOSTNAME;
-    console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+    console.log ("Started at:",startTime, "| Running On: " ,host, "\n" );
 });
